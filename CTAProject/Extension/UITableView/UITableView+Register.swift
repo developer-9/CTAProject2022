c7dd4b4147024423398b2d6cd302b93a7828c34c
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-01-15 12:49:25 +0900

    Add tableView extensions

---
+++ CTAProject/Extension/UITableView/UITableView+Register.swift
@@ -0,0 +1,23 @@
+//
+//  UITableView+Register.swift
+//  CTAProject
+//
+//  Created by Taisei Sakamoto on 2022/01/14.
+//
+
+import Foundation
+import UIKit
+
+extension UITableView {
+    
+    func register<T: UITableViewCell>(cellType: T.Type) {
+        register(cellType.self, forCellReuseIdentifier: cellType.reuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath, cellType: T.Type) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: cellType.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("DEBUG: \(cellType.self)のキャストに失敗しました。")
+        }
+        return cell
+    }
+}
