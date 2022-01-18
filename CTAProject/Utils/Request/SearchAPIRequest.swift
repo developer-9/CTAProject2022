6622e6ae47e5f8bab4fa69b98ae8bd01fa628708
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-01-18 23:30:50 +0900

    Fix typo

--- CTAProject/Utils/Request/SearchAPIRequest.swift
+++ CTAProject/Utils/Request/SearchAPIRequest.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum HotPapperApiVersion: String {
+enum HotPepperApiVersion: String {
     case v1 = "/hotpepper/gourmet/v1/"
 }
 
@@ -18,9 +18,9 @@ struct SearchAPIRequest: Requestable {
     
     var url: URL {
         var baseUrl = URLComponents(string: "https://webservice.recruit.co.jp")!
-        baseUrl.path = HotPapperApiVersion.v1.rawValue
+        baseUrl.path = HotPepperApiVersion.v1.rawValue
         baseUrl.queryItems = [
-            URLQueryItem(name: "key", value: Key.hotPapperApi),
+            URLQueryItem(name: "key", value: Key.hotPepperApi),
             URLQueryItem(name: "format", value: "json"),
             URLQueryItem(name: "count", value: "30"),
             URLQueryItem(name: "keyword", value: keyword)
