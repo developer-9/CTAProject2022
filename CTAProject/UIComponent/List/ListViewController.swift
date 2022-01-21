9f3f03a73c1518bc02702f70b381ff12448570a5
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-01-21 16:32:44 +0900

    Implement searchShops function

--- CTAProject/UIComponent/List/ListViewController.swift
+++ CTAProject/UIComponent/List/ListViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import PKHUD
 
 final class ListViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let repository: SearchRepository
+    
     private let tableView = UITableView()
+    private var shops = [Shop]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
     
     // MARK: - Lifecycle
+    
+    init(repository: SearchRepository = SearchRepositoryImpl()) {
+        self.repository = repository
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +43,21 @@ final class ListViewController: UIViewController {
     
     // MARK: - Helpers
     
+    private func searchShops(with keyword: String) {
+        HUD.show(.progress)
+        repository.searchShops(with: keyword) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let shopResponse):
+                self.shops = shopResponse.results.shop
+            case .failure(let error):
+                print("DEBUG: Error \(error.description)")
+            }
+            DispatchQueue.main.async {
+                HUD.hide()
+            }
+        }
+    }
     private func configureTableView() {
         tableView.delegate = self
         tableView.dataSource = self
@@ -48,11 +82,12 @@ extension ListViewController: UITableViewDelegate {
 
 extension ListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return shops.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(for: indexPath, cellType: ShopTableViewCell.self)
+        cell.listViewModel = ListViewModel(shop: shops[indexPath.row])
         return cell
     }
 }
