4dba0f405842d284577d389e61861694b6d187d5
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-02-05 13:27:47 +0900

    Add Mockolo(ViewModel)

--- CTAProject/UILogic/ViewModel/ListViewModel.swift
+++ CTAProject/UILogic/ViewModel/ListViewModel.swift
@@ -24,6 +24,7 @@ protocol ListViewModelOutput {
     var dataSource: Observable<[ShopResponseSectionModel]> { get }
 }
 
+/// @mockable
 protocol ListViewModelType {
     var inputs: ListViewModelInput { get }
     var outputs: ListViewModelOutput { get }
