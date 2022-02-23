82a4f6635e9c56aaaa74b1374ae3c2143b634094
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-02-23 23:32:37 +0900

    Add APIError

---
+++ CTAProject/Model/API/APIError.swift
@@ -0,0 +1,15 @@
+//
+//  APIError.swift
+//  CTAProject
+//
+//  Created by Taisei Sakamoto on 2022/02/15.
+//
+
+import Foundation
+import Moya
+
+enum APIError: Error {
+    case decode
+    case response(MoyaError)
+    case mock
+}
