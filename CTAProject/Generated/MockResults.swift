3c9819cff66c2893b7db5e816925b1b4767c84d8
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-03-05 23:44:50 +0900

    Add MockResults

--- CTAProject/Generated/MockResults.swift
+++ CTAProject/Generated/MockResults.swift
@@ -7,7 +7,23 @@
 import Foundation
 import Moya
 import PKHUD
+import RxCocoa
 import RxSwift
+import Unio
 
 
+class HotpepperRepositoryTypeMock: HotpepperRepositoryType {
+    init() { }
+
+
+    private(set) var searchRequestCallCount = 0
+    var searchRequestHandler: ((String) -> (Single<[Shop]>))?
+    func searchRequest(keyword: String) -> Single<[Shop]> {
+        searchRequestCallCount += 1
+        if let searchRequestHandler = searchRequestHandler {
+            return searchRequestHandler(keyword)
+        }
+        fatalError("searchRequestHandler returns can't have a default value thus its handler must be set")
+    }
+}
 
